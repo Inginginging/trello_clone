5f1120350db8250df53a60900dcb592534a03e19
ing928 <kik928@nate.com>
2022-04-04 15:33:18 +0900

    useform (register를 이용한 다중 input 처리)

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function ToDoList() {
+/* function ToDoList() {
   const [toDo, setToDo] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     setToDo(event.currentTarget.value);
@@ -13,6 +14,23 @@ function ToDoList() {
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} placeholder="write a to do" />
+      </form>
+    </div>
+  );
+} */
+
+function ToDoList() {
+  //여러개의 input을 하나의 객체로 보여줌 => form이 복잡할때 유용
+  const { register, watch } = useForm();
+  console.log(watch());
+  return (
+    <div>
+      <form>
+        <input {...register("email")} placeholder="email" />
+        <input {...register("id")} placeholder="id" />
+        <input {...register("pw")} placeholder="pw" />
+        <input {...register("pw2")} placeholder="pw2" />
+        <input {...register("location")} placeholder="location" />
       </form>
     </div>
   );
