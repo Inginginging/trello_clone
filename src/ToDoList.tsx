a536f849c1deb551866df68c9864e4557a008ecd
ing928 <kik928@nate.com>
2022-04-04 15:53:54 +0900

    Form Validation & Error handling

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -19,18 +19,61 @@ import { useForm } from "react-hook-form";
   );
 } */
 
+interface IForm {
+  email: string;
+  id: string;
+  pw: string;
+  pw2: string;
+  location: string;
+}
+
 function ToDoList() {
   //여러개의 input을 하나의 객체로 보여줌 => form이 복잡할때 유용
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm<IForm>();
+  //form이 valid할때 실행되는 함수. handleSubmit의 인자로 들어감.
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  //formState: form의 상태를 보여주는 메서드
+  console.log(formState.errors);
   return (
     <div>
-      <form>
-        <input {...register("email")} placeholder="email" />
-        <input {...register("id")} placeholder="id" />
-        <input {...register("pw")} placeholder="pw" />
-        <input {...register("pw2")} placeholder="pw2" />
-        <input {...register("location")} placeholder="location" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", { required: "email is required" })}
+          placeholder="email"
+        />
+        <span>{formState.errors?.email?.message}</span>
+        <input
+          {...register("id", {
+            required: "Write HERE",
+            minLength: {
+              value: 5,
+              message: "id is too short",
+            },
+          })}
+          placeholder="id"
+        />{" "}
+        <span>{formState.errors?.id?.message}</span>
+        <input
+          {...register("pw", { required: "Write HERE" })}
+          placeholder="pw"
+        />{" "}
+        <span>{formState.errors?.pw?.message}</span>
+        <input
+          {...register("pw2", { required: "Write HERE" })}
+          placeholder="pw2"
+        />{" "}
+        <span>{formState.errors?.pw2?.message}</span>
+        <input
+          {...register("location", { required: "Write HERE" })}
+          placeholder="location"
+        />{" "}
+        <span>{formState.errors?.location?.message}</span>
+        <button>Add</button>
       </form>
     </div>
   );
