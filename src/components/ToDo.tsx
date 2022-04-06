d3d326891d376db841b059e70687a2e8eb2c4dc2
ing928 <kik928@nate.com>
2022-04-06 18:41:44 +0900

    Change Category of ToDo (toDoState 배열 slice)

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,8 +1,21 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
+  //toDoState를 set하기 위한 setToDos
+  const setToDos = useSetRecoilState(toDoState);
   const onClick = (newCategory: IToDo["category"]) => {
-    console.log("i wanna go to", newCategory);
+    setToDos((oldToDos) => {
+      //click된 toDo의 index
+      const targetIdx = oldToDos.findIndex((toDo) => toDo.id === id);
+      //새로운 category로 바뀐 toDo
+      const newToDo = { text, id, category: newCategory };
+      return [
+        ...oldToDos.slice(0, targetIdx),
+        newToDo,
+        ...oldToDos.slice(targetIdx + 1),
+      ];
+    });
   };
   return (
     <li>
