9e99d6639ef07e8a973eb130108ad90b4c7ac4f7
ing928 <kik928@nate.com>
2022-04-07 17:56:54 +0900

    Selector SET

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import { hourSelector, minuteState } from "./atoms";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
+  //selector를 useState로 받는다면 첫번째 반환값은 get func값. 두번째 반환값은 set func 값.
+  const [hours, setHours] = useRecoilState(hourSelector);
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value);
   };
-  const hours = useRecoilValue(hourSelector);
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value); //selector의 set의 newValue값으로 넘어감.
+  };
   return (
     <div>
       <input
@@ -16,7 +20,12 @@ function App() {
         placeholder="minutes"
         type="number"
       />
-      <input value={hours} placeholder="hours" type="number" />
+      <input
+        value={hours}
+        onChange={onHoursChange}
+        placeholder="hours"
+        type="number"
+      />
     </div>
   );
 }
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -5,10 +5,16 @@ export const minuteState = atom({
   default: 0,
 });
 
-export const hourSelector = selector({
+export const hourSelector = selector<number>({
   key: "hours",
   get: ({ get }) => {
     const minutes = get(minuteState);
     return minutes / 60;
+  },
+  //set property는 set함수와 set할 새로운 값을 인자로 받음
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    //set 함수는 state와 새로운 값으로 setting할 값을 받음.
+    set(minuteState, minutes);
   },
 });
