f8f70adae22bb884cb4e36f4b6bd6b73f8644447
ing928 <kik928@nate.com>
2022-04-07 17:38:07 +0900

    Selector GET

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,36 +1,14 @@
 import { atom, selector } from "recoil";
 
-//category의 type을 열거형으로 지정해주는 enum
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-
-//Todo의 type
-export interface IToDo {
-  text: string;
-  id: number;
-  category: Categories;
-}
-
-//ToDos의 state. 처음엔 빈 배열. IToDo형식의 객체가 추가됨.
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
 
-export const categoryState = atom({
-  key: "category",
-  default: Categories.TO_DO,
-});
-
-export const toDoSelector = selector({
-  key: "toDoSelector",
+export const hourSelector = selector({
+  key: "hours",
   get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    //조건을 만족하는 toDo들의 배열로 return.=>derived state(파생된 state)
-    return toDos.filter((toDo) => toDo.category === category);
+    const minutes = get(minuteState);
+    return minutes / 60;
   },
 });
