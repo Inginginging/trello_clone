b03eaea5a592c125c5c15ce31ddf0858ae08cc84
ing928 <kik928@nate.com>
2022-04-07 19:34:35 +0900

    Basic of dnd (DragDropContext, Droppable, Draggable, provided)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
 function App() {
-  return <div></div>;
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {(provided) => (
+            /* provided는 dnd에서 제공하는 drag n drop을 위한 property*/
+            <ul ref={provided.innerRef} {...provided.droppableProps}>
+              <Draggable draggableId="first" index={0}>
+                {(provided) => (
+                  <li {...provided.draggableProps} ref={provided.innerRef}>
+                    <span {...provided.dragHandleProps}>❤</span>
+                    one
+                  </li>
+                )}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {(provided) => (
+                  <li {...provided.draggableProps} ref={provided.innerRef}>
+                    <span {...provided.dragHandleProps}>❤</span>
+                    two
+                  </li>
+                )}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
 }
 
 export default App;
